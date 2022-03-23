739e58bd1bd9bb0393e6ec17780383ec750fa4e1
GiuseppeCalderonio <giuseppe.calderonio@mail.polimi.it>
2022-03-23 20:22:34 +0100

    various updates

--- src/test/software/test.c
+++ src/test/software/test.c
@@ -1,15 +1,26 @@
+
 #include <stdio.h>
-#include <rocc.h>
+#include "rocc.h"
 
 int main(){
 
-    int a;
+    int a = 0;
 
-    printf("Start simulation");
+    printf("Start simulation\n");
 
-    a = ROCC_INSTRUCTION_DSS(0, 2, 4, 3, 0);
+    ROCC_INSTRUCTION_DSS(0, a, 4, 3, 0);
 
-    printf("End simulation, result is %d", a);
+    printf("End simulation, result is %d \n", a);
 
     return a;
 }
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
